4323c94c8166610b6537f5dc462372f657bdef0c
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-02-16 01:21:55 +0200

    Add Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
-import css from './Home.module.css'
+import { NavLink } from "react-router-dom";
+import css from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={css.containerHome}>
-      Home
+      <h1 className={css.mainTitle}>Campers of your dreams</h1>
+      <p className={css.mainDescription}>
+        You can find everything you want in our catalog
+      </p>
+      <NavLink className={css.mainButtonLink} to="/catalog">
+        View Now
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
