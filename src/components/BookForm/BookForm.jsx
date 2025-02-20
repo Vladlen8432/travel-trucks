752dea04bc1c0d526e6db96d99e675ad9f79f718
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-02-20 14:05:39 +0200

    Add reviews

---
+++ src/components/BookForm/BookForm.jsx
@@ -0,0 +1,7 @@
+import css from "./BookForm.module.css";
+
+const BookForm = () => {
+  return <div className={css.containerForm}>Book Form</div>;
+};
+
+export default BookForm;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,79 @@
+import PropTypes from "prop-types";
+
+import { fetchCamperById } from "../../services/api";
+import { StarIcon } from "../Icons";
 import css from "./Reviews.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Reviews = () => {
-  return <div className={css.containerReviews}>Here will be Reviews</div>;
+  const { id } = useParams();
+  const [camper, setCamper] = useState(null);
+
+  useEffect(() => {
+    fetchCamperById(id)
+      .then((data) => setCamper(data))
+      .catch((error) => console.error(error));
+  }, [id]);
+
+  if (!camper) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className={css.container}>
+      <div className={css.containerReviews}>
+        <ul className={css.reviewsList}>
+          {Array.isArray(camper.reviews) && camper.reviews.length > 0 ? (
+            camper.reviews.map(
+              ({ reviewer_name, reviewer_rating, comment }, index) => (
+                <li key={index} className={css.reviewsItem}>
+                  <div className={css.containerUser}>
+                    <div className={css.firstLetter}>
+                      {reviewer_name.charAt(0)}
+                    </div>
+
+                    <div className={css.userInfo}>
+                      <p className={css.userName}>{reviewer_name}</p>
+
+                      <ul className={css.userRatingList}>
+                        <li className={css.userRatingItem}>
+                          {[...Array(5)].map((_, i) => (
+                            <StarIcon
+                              key={i}
+                              className={
+                                i < reviewer_rating
+                                  ? `${css.starIcon} ${css.starIconYellow}`
+                                  : css.starIcon
+                              }
+                            />
+                          ))}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <p className={css.userReview}>{comment}</p>
+                </li>
+              )
+            )
+          ) : (
+            <p>No reviews available</p>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      reviewer_name: PropTypes.string.isRequired,
+      reviewer_rating: PropTypes.number.isRequired,
+      comment: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Reviews;
